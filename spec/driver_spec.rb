b99f6ec950d6f005e44be446d1f9d08624c1cc86
Michal Cichra <michal@o2h.cz>
2013-02-27 22:25:01 +0100

    fixed right click spec

--- spec/driver_spec.rb
+++ spec/driver_spec.rb
@@ -991,12 +991,9 @@ describe Capybara::Webkit::Driver do
           <ul id="events"></ul>
           <script type="text/javascript">
             var events = document.getElementById("events");
-            // https://developer.mozilla.org/en-US/docs/DOM/MouseEvent
-            var RIGHT_BUTTON = 2;
             var recordEvent = function (event) {
               var element = document.createElement("li");
-              var type = event.button == RIGHT_BUTTON ? 'contextmenu' : event.type;
-              element.innerHTML = type;
+              element.innerHTML = event.type;
               events.appendChild(element);
             };
 
